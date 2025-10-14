14dbba4016b7ff0bbd50dbc8ca56973e185b5dfc
thang2k6adu <thang2k6adu@gmail.com>
2025-10-15 01:52:40 +0700

    bai tap 15/10

--- Bai1.java
+++ Bai1.java
@@ -1,37 +1,63 @@
-class HinhChuNhat {
-  private double chieuDai;
-  private double chieuRong;
+abstract class Employee {
+    protected String name;
+    protected int age;
+    protected double salary;
 
-  public HinhChuNhat(double chieuDai, double chieuRong) {
-    this.chieuDai = chieuDai;
-    this.chieuRong = chieuRong;
-  }
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
-  public double tinhChuVi() {
-    return 2 * (chieuDai + chieuRong);
-  }
+    public abstract void calculateSalary();
 
-  public double tinhDienTich() {
-    return chieuDai * chieuRong;
-  }
-
-  public void hienThiThongTin() {
-    System.out.println("Hình chữ nhật có:");
-    System.out.println("Chiều dài: " + chieuDai);
-    System.out.println("Chiều rộng: " + chieuRong);
-    System.out.println("Chu vi: " + tinhChuVi());
-    System.out.println("Diện tích: " + tinhDienTich());
-  }
+    public void displayInfo() {
+        System.out.println("Tên: " + name);
+        System.out.println("Tuổi: " + age);
+        System.out.println("Lương: " + salary);
+        System.out.println("--------------------");
+    }
 }
 
-public class Bai1 {
-  public static void main(String[] args) {
-    HinhChuNhat hcn1 = new HinhChuNhat(5, 3);
-    HinhChuNhat hcn2 = new HinhChuNhat(10, 7);
-    HinhChuNhat hcn3 = new HinhChuNhat(6.5, 4.2);
-  
-    hcn1.hienThiThongTin();
-    hcn2.hienThiThongTin();
-    hcn3.hienThiThongTin();
-  }
+class OfficeEmployee extends Employee {
+    private int workingDays;
+    private static final double DAILY_RATE = 100; // cố định
+
+    public OfficeEmployee(String name, int age, int workingDays) {
+        super(name, age);
+        this.workingDays = workingDays;
+    }
+
+    @Override
+    public void calculateSalary() {
+        salary = workingDays * DAILY_RATE;
+    }
+}
+
+class TechnicalEmployee extends Employee {
+    private int workingHours;
+    private double hourlyRate;
+
+    public TechnicalEmployee(String name, int age, int workingHours, double hourlyRate) {
+        super(name, age);
+        this.workingHours = workingHours;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public void calculateSalary() {
+        salary = workingHours * hourlyRate;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        OfficeEmployee officeEmp = new OfficeEmployee("An", 28, 22);
+        TechnicalEmployee techEmp = new TechnicalEmployee("Bình", 30, 160, 15);
+
+        officeEmp.calculateSalary();
+        techEmp.calculateSalary();
+
+        officeEmp.displayInfo();
+        techEmp.displayInfo();
+    }
 }
--- Bai2.java
+++ Bai2.java
@@ -1,116 +1,78 @@
-import java.util.*;
-
-class TaiKhoan {
-  private String soTaiKhoan;
-  private String tenChuTaiKhoan;
-  private Double soDu;
-  private static double laiSuatNam = 0.05;
-
-  public TaiKhoan(String soTaiKhoan, String tenChuTaiKhoan, double soDu) {
-    this.soTaiKhoan = soTaiKhoan;
-    this.tenChuTaiKhoan = tenChuTaiKhoan;
-    this.soDu = soDu;
-  }
-
-  public void napTien(double tien) {
-    if (tien > 0) {
-      soDu += tien;
-      System.out.println("Đã nạp: " + tien + " vào TK " + soTaiKhoan);
-    } else {
-      System.out.println("Số tiền nạp không hợp lệ!");
-    }
-  }
-
-  public void rutTien(double tien) {
-    if (tien > 0 && tien <= soDu) {
-      soDu -= tien;
-      System.out.println("Đã rút: " + tien + " từ TK " + soTaiKhoan);
-    } else {
-      System.out.println("Số tiền rút không hợp lệ hoặc vượt quá số dư!");
-    }
-  }
-
-  public double tinhLaiThang() {
-    return soDu * (laiSuatNam / 12);
-  }
-
-  public void hienThiThongTin() {
-    System.out.println("Số TK: " + soTaiKhoan);
-    System.out.println("Chủ TK: " + tenChuTaiKhoan);
-    System.out.println("Số dư: " + soDu);
-    System.out.println("Lãi suất năm: " + (laiSuatNam * 100) + "%");
-  }
-
-  public static void thayDoiLaiSuat(double laiMoi) {
-    if (laiMoi >= 0) {
-      laiSuatNam = laiMoi;
-      System.out.println("Lãi suất mới là: " + (laiSuatNam * 100) + "%");
-    } else {
-      System.out.println("Lãi suất không hợp lệ!");
-    }
-  }
-
-  public double getSoDu() {
-    return soDu;
-  }
-
-  public String getSoTaiKhoan() {
-    return soTaiKhoan;
-  }
+interface EmailSender {
+    void sendEmail();
 }
 
-public class Bai2 {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    ArrayList<TaiKhoan> danhSach = new ArrayList<>();
+interface Programmer {
+    void code();
+}
 
-    danhSach.add(new TaiKhoan("001", "Nguyen Van A", 1000));
-    danhSach.add(new TaiKhoan("002", "Tran Thi B", 5000));
-    danhSach.add(new TaiKhoan("003", "Le Van C", 3000));
+interface Salesperson {
+    void sell();
+}
 
-    danhSach.get(0).napTien(500);
-    danhSach.get(1).rutTien(1000);
+class OfficeEmployee implements EmailSender {
+    private String name;
 
-    for (TaiKhoan tk : danhSach) {
-      double laiThang = tk.tinhLaiThang();
-      tk.napTien(laiThang);
-      System.out.println("Đã cộng lãi tháng: " + laiThang + " vào TK " + tk.getSoTaiKhoan());
+    public OfficeEmployee(String name) {
+        this.name = name;
     }
 
-    System.out.println("\nDanh sách tài khoản sau giao dịch:");
-    for (TaiKhoan tk : danhSach) {
-      tk.hienThiThongTin();
+    @Override
+    public void sendEmail() {
+        System.out.println(name + " đang gửi email cho khách hàng.");
+    }
+}
+
+class TechnicalEmployee implements EmailSender, Programmer {
+    private String name;
+
+    public TechnicalEmployee(String name) {
+        this.name = name;
     }
 
-    TaiKhoan.thayDoiLaiSuat(0.06); // 6%/năm
-
-    System.out.print("\nNhập số tài khoản cần tìm: ");
-    String soTKTim = sc.nextLine();
-    boolean found = false;
-    for (TaiKhoan tk : danhSach) {
-      if (tk.getSoTaiKhoan().equals(soTKTim)) {
-        System.out.println("Tài khoản tìm thấy:");
-        tk.hienThiThongTin();
-        found = true;
-        break;
-      }
-    }
-    if (!found) {
-      System.out.println("Không tìm thấy tài khoản!");
+    @Override
+    public void sendEmail() {
+        System.out.println(name + " đang gửi email báo cáo tiến độ.");
     }
 
-    Collections.sort(danhSach, new Comparator<TaiKhoan>() {
-      @Override
-      public int compare(TaiKhoan t1, TaiKhoan t2) {
-        return Double.compare(t2.getSoDu(), t1.getSoDu());
-      }
-    });
+    @Override
+    public void code() {
+        System.out.println(name + " đang lập trình tính năng mới.");
+    }
+}
 
-    System.out.println("\nDanh sách tài khoản sau khi sắp xếp theo số dư giảm dần:");
-    for (TaiKhoan tk : danhSach) {
-      tk.hienThiThongTin();
+class SalesEmployee implements EmailSender, Salesperson {
+    private String name;
+
+    public SalesEmployee(String name) {
+        this.name = name;
     }
 
-    sc.close();
-  }
+    @Override
+    public void sendEmail() {
+        System.out.println(name + " đang gửi email chào hàng.");
+    }
+
+    @Override
+    public void sell() {
+        System.out.println(name + " đang tư vấn và chốt đơn hàng.");
+    }
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+        OfficeEmployee officeEmp = new OfficeEmployee("Lan");
+        TechnicalEmployee techEmp = new TechnicalEmployee("Minh");
+        SalesEmployee salesEmp = new SalesEmployee("Hùng");
+
+        officeEmp.sendEmail();
+        System.out.println();
+
+        techEmp.sendEmail();
+        techEmp.code();
+        System.out.println();
+
+        salesEmp.sendEmail();
+        salesEmp.sell();
+    }
 }
