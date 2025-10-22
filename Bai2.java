cc3d73ebc8adf5550cf76c521052ae1e4755483e
Tran Duc Thang <thang2k6adu@gmail.com>
2025-10-22 15:28:41 +0700

    Refactor character classes and add new types

--- Bai2.java
+++ Bai2.java
@@ -1,78 +1,93 @@
-interface EmailSender {
-    void sendEmail();
-}
+abstract class BaseCharacter {
+    protected String name;
+    protected int hp;
+    protected int baseDamage;
 
-interface Programmer {
-    void code();
-}
-
-interface Salesperson {
-    void sell();
-}
-
-class OfficeEmployee implements EmailSender {
-    private String name;
-
-    public OfficeEmployee(String name) {
+    public BaseCharacter(String name, int hp, int baseDamage) {
         this.name = name;
+        this.hp = hp;
+        this.baseDamage = baseDamage;
+    }
+
+    public abstract void attack();
+}
+
+class Warrior extends BaseCharacter {
+    public Warrior(String name, int hp, int baseDamage) {
+        super(name, hp, baseDamage);
     }
 
     @Override
-    public void sendEmail() {
-        System.out.println(name + " đang gửi email cho khách hàng.");
+    public void attack() {
+        System.out.println(name + " dùng vũ khí cận chiến tấn công! Sát thương: " + baseDamage);
     }
 }
 
-class TechnicalEmployee implements EmailSender, Programmer {
-    private String name;
-
-    public TechnicalEmployee(String name) {
-        this.name = name;
+class Archer extends BaseCharacter {
+    public Archer(String name, int hp, int baseDamage) {
+        super(name, hp, baseDamage);
     }
 
     @Override
-    public void sendEmail() {
-        System.out.println(name + " đang gửi email báo cáo tiến độ.");
+    public void attack() {
+        System.out.println(name + " bắn mũi tên từ xa! Sát thương: " + baseDamage);
+        longRangeAttack();
     }
 
-    @Override
-    public void code() {
-        System.out.println(name + " đang lập trình tính năng mới.");
+    private void longRangeAttack() {
+        System.out.println(name + " thực hiện tấn công tầm xa bổ sung!");
     }
 }
 
-class SalesEmployee implements EmailSender, Salesperson {
-    private String name;
-
-    public SalesEmployee(String name) {
-        this.name = name;
+class Mage extends BaseCharacter {
+    public Mage(String name, int hp, int baseDamage) {
+        super(name, hp, baseDamage);
     }
 
     @Override
-    public void sendEmail() {
-        System.out.println(name + " đang gửi email chào hàng.");
+    public void attack() {
+        System.out.println(name + " thi triển phép thuật! Sát thương: " + baseDamage);
+        castSpell();
     }
 
-    @Override
-    public void sell() {
-        System.out.println(name + " đang tư vấn và chốt đơn hàng.");
+    private void castSpell() {
+        System.out.println(name + " sử dụng kỹ năng phép thuật bổ sung!");
     }
 }
 
-public class Main2 {
+class ArcaneArcher extends BaseCharacter {
+    public ArcaneArcher(String name, int hp, int baseDamage) {
+        super(name, hp, baseDamage);
+    }
+
+    @Override
+    public void attack() {
+        System.out.println(name + " bắn mũi tên phép tấn công! Sát thương: " + baseDamage);
+        longRangeAttack();
+        castSpell();
+    }
+
+    private void longRangeAttack() {
+        System.out.println(name + " thực hiện tấn công tầm xa bổ sung!");
+    }
+
+    private void castSpell() {
+        System.out.println(name + " sử dụng phép thuật bổ sung!");
+    }
+}
+
+public class SimpleRPG {
     public static void main(String[] args) {
-        OfficeEmployee officeEmp = new OfficeEmployee("Lan");
-        TechnicalEmployee techEmp = new TechnicalEmployee("Minh");
-        SalesEmployee salesEmp = new SalesEmployee("Hùng");
+        BaseCharacter[] characters = new BaseCharacter[] {
+            new Warrior("Leon", 100, 15),
+            new Archer("Robin", 80, 12),
+            new Mage("Merlin", 70, 20),
+            new ArcaneArcher("Luna", 75, 14)
+        };
 
-        officeEmp.sendEmail();
-        System.out.println();
-
-        techEmp.sendEmail();
-        techEmp.code();
-        System.out.println();
-
-        salesEmp.sendEmail();
-        salesEmp.sell();
+        for (BaseCharacter character : characters) {
+            character.attack();
+            System.out.println("---");
+        }
     }
 }
